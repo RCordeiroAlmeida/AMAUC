8d823ebf5cfe04169af38ecfd451536ac313d366
Rafael <74336014+RCordeiroAlmeida@users.noreply.github.com>
2023-05-29 16:40:01 -0300

    correção de bug
    o apostrofe estava causando problemas na edição e cadastro das atividades

--- application/atividade/view/dataControls.inc.php
+++ application/atividade/view/dataControls.inc.php
@@ -32,14 +32,14 @@ switch ($_GET['acao']) {
 		$aux['afr_cod']				= $_POST['afr_cod'];
 		$aux['sol_status']          = $_POST['sol_status'];
 		$aux['ati_tempo']          	= $_POST['ati_tempo'];
-		$aux['ati_descricao']       = $_POST['ati_descricao'];
+		$aux['ati_descricao']       = addslashes(mb_strtoupper($_POST['ati_descricao'], 'UTF-8'));
 
 		$data->tabela = 'atividade';
 		$data->update($aux);
 
 		$sql= 'UPDATE solicitacao SET sol_status ='.$aux['sol_status']." WHERE sol_status = 1 AND sol_cod =". $_POST['sol_cod'];
-
 		$data->executaSQL($sql);
+		
 
 		echo '<script>nextPage("?module=atividade&acao=lista&ms=1", '.$_POST['sol_cod'].');</script>';
 		break;
